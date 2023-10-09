f5b4f9b3994266fa0c6b19d85aad461cb26938d3
bk7312 <bk7312@users.noreply.github.com>
2023-10-09 12:02:11 +0800

    update to openai 4
    apparently, still using serverless, openai sdk v4 required to use edge, also removed streaming for now

--- api/openai.js
+++ api/openai.js
@@ -1,9 +1,7 @@
-import { Configuration, OpenAIApi } from 'openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { OpenAI } from 'openai'
+// import { OpenAIStream, StreamingTextResponse } from 'ai'
 
-const openai = new OpenAIApi(new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-}))
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
 export const runtime = 'edge'
 
@@ -12,22 +10,22 @@ export default async function handler(req, res) {
     const messages = req.body
 
     try {
-        const response = await openai.createChatCompletion({
+        const response = await openai.chat.completions.create({
             model,
-            stream: true,
+            // stream: true,
             messages,
         })
         console.log("stream-response:", response)
 
-        // Convert the response into a friendly text-stream
-        const stream = OpenAIStream(response)
+        // // Convert the response into a friendly text-stream
+        // const stream = OpenAIStream(response)
 
-        // Respond with the stream
-        return new StreamingTextResponse(stream)
+        // // Respond with the stream
+        // return new StreamingTextResponse(stream)
 
-        // return res.status(200).json({
-        //     reply: response.data.choices[0].message
-        // })
+        return res.status(200).json({
+            reply: response.choices[0].message
+        })
     } catch (error) {
         console.log("error:", error)
         return res.status(500).json("Something went wrong...")
