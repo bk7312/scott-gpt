f76ca7bd77dea3aac89a934bb16dadf3be3f5756
bk7312 <bk7312@users.noreply.github.com>
2023-10-09 11:30:14 +0800

    removed dotenv and change runtime to edge

--- api/openai.js
+++ api/openai.js
@@ -1,15 +1,15 @@
-import * as dotenv from 'dotenv'
 import { Configuration, OpenAIApi } from 'openai'
-
-dotenv.config()
 
 const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 }))
 
+export const runtime = 'edge'
+
 export default async function handler(req, res) {
     const model = "gpt-3.5-turbo"
     const messages = req.body
+
     try {
         const response = await openai.createChatCompletion({
             model,
