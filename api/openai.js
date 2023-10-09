57cf576136b7f0a0b0cfcdf48c778a5792415c05
bk7312 <bk7312@users.noreply.github.com>
2023-10-09 11:50:32 +0800

    using vercel ai sdk streaming response

--- api/openai.js
+++ api/openai.js
@@ -1,4 +1,5 @@
 import { Configuration, OpenAIApi } from 'openai'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
 
 const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -13,14 +14,23 @@ export default async function handler(req, res) {
     try {
         const response = await openai.createChatCompletion({
             model,
-            messages
+            stream: true,
+            messages,
         })
-        console.log("response:", response)
-        return res.status(200).json({
-            reply: response.data.choices[0].message
-        })
+        console.log("stream-response:", response)
+
+        // Convert the response into a friendly text-stream
+        const stream = OpenAIStream(response)
+
+        // Respond with the stream
+        return new StreamingTextResponse(stream)
+
+        // return res.status(200).json({
+        //     reply: response.data.choices[0].message
+        // })
     } catch (error) {
         console.log("error:", error)
         return res.status(500).json("Something went wrong...")
     }
 }
+
