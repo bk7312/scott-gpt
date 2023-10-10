d5f27385966848e1a5b323ea8a9e4b425b993ad0
bk7312 <bk7312@users.noreply.github.com>
2023-10-10 14:23:59 +0800

    working locally in edge runtime

--- api/openai.js
+++ api/openai.js
@@ -3,32 +3,42 @@ import { OpenAI } from 'openai'
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
-export const runtime = 'edge'
+export const config = {
+    runtime: "edge",
+};
 
-export default async function handler(req, res) {
-    const model = "gpt-3.5-turbo"
-    const messages = req.body
+export default async function handler(req) {
+    if (req.method === 'GET') {
+        return new Response.redirect(window.location.origin)
+    }
+    console.log('test', { req })
 
     try {
+        const body = await req.json()
+
         const response = await openai.chat.completions.create({
-            model,
+            model: "gpt-3.5-turbo",
             // stream: true,
-            messages,
+            messages: body,
         })
         console.log("stream-response:", response)
 
         // // Convert the response into a friendly text-stream
         // const stream = OpenAIStream(response)
-
+        // console.log(stream)
         // // Respond with the stream
         // return new StreamingTextResponse(stream)
 
-        return res.status(200).json({
-            reply: response.choices[0].message
-        })
+        const reply = JSON.stringify(response.choices[0].message)
+
+        return new Response(reply, { status: 200 })
+        // return res.status(200).json({
+        //     reply: response.choices[0].message
+        // })
     } catch (error) {
         console.log("error:", error)
-        return res.status(500).json("Something went wrong...")
+        return new Response('Something went wrong...', { status: 500 })
+        // return res.status(500).json("Something went wrong...")
     }
 }
 
--- script.js
+++ script.js
@@ -70,17 +70,30 @@ const handleSubmit = async e => {
             },
             body: JSON.stringify(messages)
         })
+        console.log('fetchresp', response)
 
-        if (response.ok) {
-            const data = await response.json()
-            messages.push(data.reply)
-            addMessage("ScottGPT", data.reply.content)
-            waiting(false)
-            $prompt.focus({ preventScroll: true })
-        } else {
+        if (!response.ok) {
             const err = await response.text()
             throw new Error(err)
         }
+
+        // const reader = response.body.getReader()
+        // const decoder = new TextDecoder('utf-8')
+        // let reply = ''
+        // while (true) {
+        //     const { value, done } = await reader.read()
+        //     if (done) break
+        //     const decodedValue = decoder.decode(value)
+        //     console.log({ value, decodedValue })
+        //     reply += decodedValue
+        // }
+        const reply = await response.json()
+        console.log(reply)
+        messages.push(reply)
+        addMessage("ScottGPT", reply.content)
+        waiting(false)
+        $prompt.focus({ preventScroll: true })
+
     } catch (error) {
         addMessage("SYSTEM", error)
     }
