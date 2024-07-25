d87e14a85c338a7c9cc4b212313fe3121260e4b1
bk7312 <bk7312@users.noreply.github.com>
2024-07-25 10:20:47 +0800

    Update model to gpt-4o-mini

--- api/openai.js
+++ api/openai.js
@@ -1,36 +1,35 @@
-import { OpenAI } from 'openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { OpenAI } from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export const config = {
-    runtime: "edge",
+  runtime: 'edge',
 };
 
 const system = {
-    role: "system",
-    content: "You are Scott, a Singaporean, reply in Singlish. If you don't know how to respond, make a joke about it."
-}
+  role: 'system',
+  content:
+    "You are Scott, a Singaporean, reply in Singlish. If you don't know how to respond, make a joke about it.",
+};
 
 export default async function handler(req) {
-    if (req.method === 'GET') {
-        return Response.redirect(`http://${req.headers.get('host')}`)
-    }
+  if (req.method === 'GET') {
+    return Response.redirect(`http://${req.headers.get('host')}`);
+  }
 
-    try {
-        const body = await req.json()
-        const messages = [system, ...body]
-        const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            stream: true,
-            messages,
-        })
-        const stream = OpenAIStream(response)
-        return new StreamingTextResponse(stream)
-
-    } catch (error) {
-        console.log("error:", error)
-        return new Response('Something went wrong...', { status: 500 })
-    }
+  try {
+    const body = await req.json();
+    const messages = [system, ...body];
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      stream: true,
+      messages,
+    });
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.log('error:', error);
+    return new Response('Something went wrong...', { status: 500 });
+  }
 }
-
